9f87ffa416ec279411abddc012c23abe3eb6ffb4
Wenxuan Bao <baowenxuan@gmail.com>
2025-07-21 22:44:24 -0400

    a simple MLP model

--- src/model/MLP.py
+++ src/model/MLP.py
@@ -4,8 +4,48 @@ import torch.nn.functional as F
 
 
 class MLP(nn.Module):
-    def __init__(self):
+    def __init__(
+            self,
+            n_nodes: int,
+            n_feats: int = 4,
+            n_outputs: int = 4,
+            hidden_dim: int = 64,
+            n_layers: int = 2,
+            activation: str = 'relu',
+            dropout: float = 0.0,
+    ):
         super().__init__()
+        self.n_nodes = n_nodes
+        self.n_feats = n_feats
+        self.n_outputs = n_outputs
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+
+        input_dim = n_nodes * n_feats
+        output_dim = n_nodes * n_outputs
+
+        if activation == 'relu':
+            activation = nn.ReLU()
+        else:
+            raise NotImplementedError
+
+        assert n_layers >= 2
+
+        layers = []
+        layers.append(nn.Linear(input_dim, hidden_dim))
+        layers.append(activation)
+        for _ in range(n_layers - 2):
+            layers.append(nn.Linear(hidden_dim, hidden_dim))
+            layers.append(activation)
+
+        layers.append(nn.Linear(hidden_dim, output_dim))
+
+        self.net = nn.Sequential(*layers)
+
 
     def forward(self, x):
-        pass
+        B = x.shape[0]
+        x = x.view(B, -1)
+        out = self.net(x)
+        out = out.view(B, self.n_nodes, self.n_outputs)
+        return out
